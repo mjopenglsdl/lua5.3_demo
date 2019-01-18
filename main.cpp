459d5f5e616a557031b68627ec46bcce889839a1
Minjie <mjopenglsdl@gmail.com>
2019-01-18 15:17:39 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+#include <lua5.3/lua.hpp>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    // blocks until program ends
+    int err = luaL_loadfile(L, "../test.lua") || lua_pcall(L, 0, 0, 0);
+
+    if(err){
+        std::string str = lua_tostring(L, -1);
+        cout<<"err: "<<str<<endl;
+        lua_pop(L, 1);
+    }    
+
+    lua_close(L);
+
+    cout<<"-------END-------"<<endl;
+    return 0;
+}
---
+++ test.lua
@@ -0,0 +1,9 @@
+print("start +++")
+counter = 0
+
+while(counter<10000)
+do
+    counter = counter + 1 print("counter: ", counter)
+end
+
+print("stop ---")
